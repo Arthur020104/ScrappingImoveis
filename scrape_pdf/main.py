ed4113136305f60947e4ad09a01f737e08388ffb
Arthur020104 <arthur020104@gmail.com>
2024-07-11 11:22:42 -0300

    Preparando arquivos para integração

--- scrape_pdf/main.py
+++ scrape_pdf/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from .download import download_pdf  # Supondo que você tenha um módulo chamado download
-from .infoExtract import read_pdf, extract_info  # Supondo que você tenha esses módulos
+from .download import download_pdf  # Assumindo que você tem um módulo chamado download
+from .infoExtract import read_pdf, extract_info  # Assumindo que você tem esses módulos
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import threading
 import os
@@ -17,18 +17,8 @@ def initialize_directories():
         os.makedirs("temp")
 
 
-def read_original_dataframe(filepath, num_rows=25):
-    """
-    Lê o DataFrame original a partir de um arquivo CSV, removendo duplicatas.
-
-    Args:
-        filepath (str): Caminho para o arquivo CSV.
-        num_rows (int): Número de linhas a serem lidas do arquivo.
-
-    Returns:
-        pd.DataFrame: DataFrame lido e filtrado.
-    """
-    df = pd.read_csv(filepath)[:num_rows]
+def read_original_dataframe(filepath):
+    df = pd.read_csv(filepath)
     df = df.drop_duplicates(subset='Insc_Cadastral')
     return df
 
@@ -41,26 +31,26 @@ def wait_before_start(n=3):
         n (int): Número de segundos para aguardar.
     """
     for i in range(n):
-        print(f"Waiting {n - i} seconds...")
+        print(f"Aguardando {n - i} segundos...")
         time.sleep(1)
-    print("Starting")
+    print("Iniciando")
 
 
 def process_item(item):
     """
-    Processa um único item, baixando o PDF e extraindo as informações.
+    Processa um único item baixando o PDF e extraindo informações.
 
     Args:
         item (dict): Dicionário contendo 'index' e 'Insc_Cadastral'.
 
     Returns:
-        tuple or None: Índice e informações extraídas ou None em caso de falha.
+        tuple or None: Índice e informações extraídas, ou None em caso de falha.
     """
     number = item["Insc_Cadastral"].replace(' ', '')
     try:
         response = download_pdf(number)
     except Exception as e:
-        print(f"Error downloading PDF for {number}: {e}")
+        print(f"Erro ao baixar PDF para {number}: {e}")
         return None
 
     if response:
@@ -75,11 +65,11 @@ def process_item(item):
 
 def save_and_clear(df, cycle_number):
     """
-    Salva o DataFrame em arquivos CSV e Excel, limpando-o em seguida.
+    Salva o DataFrame em arquivos CSV e Excel, depois limpa-o.
 
     Args:
         df (pd.DataFrame): DataFrame a ser salvo.
-        cycle_number (int): Número do ciclo de salvamento.
+        cycle_number (int): Número do ciclo para salvar.
     """
     df.to_csv(f"temp/BaseConsolidada_cycle{cycle_number}.csv", index=False)
     df.to_excel(f"temp/BaseConsolidada_cycle{cycle_number}.xlsx", index=False)
@@ -88,53 +78,44 @@ def save_and_clear(df, cycle_number):
 
 def process_dataframe_chunks(df_original, save_number, concurrent_threads):
     """
-    Processa o DataFrame original em chunks, salvando os resultados em ciclos.
+    Processa o DataFrame original em pedaços, salvando os resultados em ciclos.
 
     Args:
         df_original (pd.DataFrame): DataFrame original a ser processado.
-        save_number (int): Número de registros por chunk.
+        save_number (int): Número de registros por pedaço.
         concurrent_threads (int): Número de threads simultâneas.
 
     Returns:
         int: Último ciclo processado.
     """
     num_chunks = len(df_original) // save_number + (len(df_original) % save_number > 0)
-    all_results = []
     last_cycle = -1
 
     for cycle in range(num_chunks):
         start_time_cycle = time.time()
-        print(f"Starting cycle {cycle + 1}")
+        print(f"Iniciando ciclo {cycle + 1}")
         start_index = cycle * save_number
         end_index = min((cycle + 1) * save_number, len(df_original))
         df_chunk = df_original.iloc[start_index:end_index]
 
-        current_threads = []
-
-        with ThreadPoolExecutor(max_workers=concurrent_threads) as executor:
-            for index, row in df_chunk.iterrows():
-                item = {"index": index, "Insc_Cadastral": row["Insc_Cadastral"]}
-                current_threads.append(executor.submit(process_item, item))
-
-        for thread in threading.enumerate():
-            if thread != threading.current_thread():
-                thread.join()
-
-        for future in as_completed(current_threads):
-            result = future.result()
-            if result is not None:
-                index, info = result
-                all_results.append(info)
-
-        save_cycle_results(all_results, cycle)
-        last_cycle += 1
-        combined_df = combine_csv_files(last_cycle)
-        merge_and_save_intermediate_dataframe(combined_df, df_original, cycle)
         all_results = []
 
+        with ThreadPoolExecutor(max_workers=concurrent_threads) as executor:
+            futures = {executor.submit(process_item, {"index": index, "Insc_Cadastral": row["Insc_Cadastral"]}): index for index, row in df_chunk.iterrows()}
+
+            for future in as_completed(futures):
+                result = future.result()
+                if result is not None:
+                    _, info = result
+                    all_results.append(info)
+        last_cycle += 1
+        save_cycle_results(all_results, last_cycle)
+        #combined_df = combine_csv_files(last_cycle)
+        #merge_and_save_intermediate_dataframe(combined_df, df_original, cycle)
+
+        #clear_temp_folders()
         end_time_cycle = time.time()
-        clear_temp_folders()
-        print(f"Cycle {cycle + 1} completed in {end_time_cycle - start_time_cycle:.2f} seconds")
+        print(f"Ciclo {cycle + 1} concluído em {end_time_cycle - start_time_cycle:.2f} segundos")
 
     return last_cycle
 
@@ -145,15 +126,15 @@ def save_cycle_results(results, cycle):
 
     Args:
         results (list): Lista de resultados a serem salvos.
-        cycle (int): Número do ciclo de salvamento.
+        cycle (int): Número do ciclo para salvar.
     """
     df = pd.DataFrame(results)
     csv_filename = f'temp/BaseConsolidada_output_{cycle}.csv'
-    excel_filename = f'temp/BaseConsolidada_output_{cycle}.xlsx'
+    #excel_filename = f'temp/BaseConsolidada_output_{cycle}.xlsx'
     df.to_csv(csv_filename, index=False)
-    df.to_excel(excel_filename, index=False)
-    print(f"CSV file created: {csv_filename}")
-    print(f"Excel file created: {excel_filename}")
+    # df.to_excel(excel_filename, index=False)
+    print(f"Arquivo CSV criado: {csv_filename}")
+    #print(f"Arquivo Excel criado: {excel_filename}")
 
 
 def combine_csv_files(last_cycle):
@@ -170,7 +151,7 @@ def combine_csv_files(last_cycle):
     for i in range(last_cycle + 1):
         os.remove(f"temp/BaseConsolidada_output_{i}.csv")
     combined_df.to_csv("temp/BaseConsolidada_combined.csv", index=False)
-    print("Combined CSV file created: temp/BaseConsolidada_combined.csv")
+    print("Arquivo CSV combinado criado: temp/BaseConsolidada_combined.csv")
     return combined_df
 
 
@@ -187,7 +168,7 @@ def merge_and_save_intermediate_dataframe(combined_df, df_original, cycle):
 
     selected_columns = [
         'Cod_Reduzido', 'Insc_Cadastral', 'Imovel_Endereco', 'Bairro', 'Quadra', 'Lote',
-        'Area Territorial', 'Area Predial', 'Testada', 'Cod_Prefeitura', 'Contribuinte_CPF',
+        'AreaTerritorial', 'AreaPredial', 'Testada_y', 'Cod_Prefeitura', 'Contribuinte_CPF',
         'Contribuinte_Nome', 'Contribuinte_Endereco', 'Contribuinte_CEP', 'Bairro_Contribuinte'
     ]
 
@@ -203,23 +184,25 @@ def merge_and_save_intermediate_dataframe(combined_df, df_original, cycle):
 
     final_columns = [
         'Cod_Reduzido', 'Insc_Cadastral', 'Imovel_Endereco', 'Bairro', 'Quadra', 'Lote',
-        'Area Territorial', 'Area Predial', 'Testada', 'Cod_Prefeitura', 'Contribuinte_CPF',
+        'AreaTerritorial', 'AreaPredial', 'Testada_y', 'Cod_Prefeitura', 'Contribuinte_CPF',
         'Contribuinte_Nome', 'Contribuinte_Endereco', 'Contribuinte_CEP', 'Bairro_Contribuinte', 'CEPImovel'
     ]
     final_columns = [col for col in final_columns if col in merged_df.columns]
 
     final_df = merged_df[final_columns]
     final_df.to_csv(f"temp/Final_Combined_cycle{cycle}.csv", index=False)
-    final_df.to_excel(f"temp/Final_Combined_cycle{cycle}.xlsx", index=False)
-    print(f"Final combined CSV file for cycle {cycle} created: temp/Final_Combined_cycle{cycle}.csv")
+    #final_df.to_excel(f"temp/Final_Combined_cycle{cycle}.xlsx", index=False)
+    print(f"Arquivo CSV final combinado para o ciclo {cycle} criado: temp/Final_Combined_cycle{cycle}.csv")
+    # O retorno deve ser o caminho absoluto do novo arquivo
+    return os.path.abspath(f"temp/Final_Combined_cycle{cycle}.csv")
 
 
-def process_chunk_pdf(csv_filepath:str, concurrent_threads:int):
+def process_chunk_pdf(csv_filepath, concurrent_threads):
     """
-    Função que coordena a execução de todas as etapas do processamento de chunks de PDFs.
+    Coordena a execução de todas as etapas do processamento de pedaços de PDFs.
 
     Args:
-        csv_filepath (str): Caminho para o arquivo CSV que será processado.
+        csv_filepath (str): Caminho para o arquivo CSV a ser processado.
         concurrent_threads (int): Número de threads simultâneas a serem usadas.
     """
     start_time = time.time()
@@ -227,26 +210,27 @@ def process_chunk_pdf(csv_filepath:str, concurrent_threads:int):
     # Inicializa os diretórios necessários
     initialize_directories()
     
-    # Lê o DataFrame original do CSV
+    # Lê o DataFrame original a partir do CSV
     df_original = read_original_dataframe(csv_filepath)
     
     # Aguarda alguns segundos antes de iniciar o processamento
     wait_before_start()
     
-    # Processa os chunks do DataFrame original usando as threads concorrentes
+    # Processa os pedaços do DataFrame original usando threads simultâneas
     last_cycle = process_dataframe_chunks(df_original, concurrent_threads, concurrent_threads)
     
     # Combina os arquivos CSV gerados em um único DataFrame
     combined_df = combine_csv_files(last_cycle)
     
     # Faz o merge do DataFrame combinado com o DataFrame original e salva o resultado
-    merge_and_save_intermediate_dataframe(combined_df, df_original, "final")
-    
+    path = merge_and_save_intermediate_dataframe(combined_df, df_original, "final")
+    clear_temp_folders()
     # Calcula e imprime o tempo total de execução
     time_taken = time.time() - start_time
-    print(f"Time taken: {time_taken:.2f} seconds")
+    print(f"Tempo total: {time_taken:.2f} segundos")
+    return path
 
 
 if __name__ == "__main__":
-    # Exemplo de chamada da função process_chunk_pdf
-    process_chunk_pdf("BaseConsolidada.csv", 15)
+    # Exemplo de chamada para a função process_chunk_pdf
+    process_chunk_pdf("C:/Users/arthu/OneDrive/Desktop/Repo/Integracao/scrape_pdf/Base.csv", 15)
